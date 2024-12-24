6eb4769ded833a3219e9796450fd248b74ae8a62
Diya <diya.c33@gmail.com>
2024-12-23 20:16:08 -0500

    Data pre-processing mostly complete. Next steps: splitting the data into train and test sets

--- bike.py
+++ bike.py
@@ -24,8 +24,8 @@ for i in df.columns:
 
 #print(df.head()) #rechecking the modified data frame
 
-print(df.columns) #checking the status of the columns after the modification
-print(df['pickup_time'])
+#print(df.columns) #checking the status of the columns after the modification
+#print(df['pickup_time'])
 
 #making the times more readable by first converting to datetime type and then splitting each
 #component into year, month, day, hour, minute, day_of_week
@@ -33,21 +33,46 @@ print(df['pickup_time'])
 df['pickup_time'] = pd.to_datetime(df['pickup_time'])
 df['drop_time'] = pd.to_datetime(df['pickup_time'])
 
-df['pickup_year'] = df['pickup_time'].dt.year
-df['pickup_month'] = df['pickup_time'].dt.month    # Extract month
-df['pickup_day'] = df['pickup_time'].dt.day        # Extract day
+##df['pickup_year'] = df['pickup_time'].dt.year
+##df['pickup_month'] = df['pickup_time'].dt.month    # Extract month
+##df['pickup_day'] = df['pickup_time'].dt.day        # Extract day
 df['pickup_hour'] = df['pickup_time'].dt.hour      # Extract hour
-df['pickup_minute'] = df['pickup_time'].dt.minute  # Extract minute
+##df['pickup_minute'] = df['pickup_time'].dt.minute  # Extract minute
 df['pickup_day_of_week'] = df['pickup_time'].dt.dayofweek  # Extract day of the week (Mon=0, Sun=6)
-
-df['drop_year'] = df['drop_time'].dt.year
-df['drop_month'] = df['drop_time'].dt.month    # Extract month
-df['drop_day'] = df['drop_time'].dt.day        # Extract day
+##
+##df['drop_year'] = df['drop_time'].dt.year
+##df['drop_month'] = df['drop_time'].dt.month    # Extract month
+##df['drop_day'] = df['drop_time'].dt.day        # Extract day
 df['drop_hour'] = df['drop_time'].dt.hour      # Extract hour
-df['drop_minute'] = df['drop_time'].dt.minute  # Extract minute
+##df['drop_minute'] = df['drop_time'].dt.minute  # Extract minute
 df['drop_day_of_week'] = df['drop_time'].dt.dayofweek  # Extract day of the week (Mon=0, Sun=6)
 
-print(df.columns)
 
-# next step: check which features should be discarded
-# df_select = df.loc[...]
+df['trip_duration'] = (df['drop_time'] - df['pickup_time']).dt.total_seconds() #column for duration of travel (in mins)
+
+x = df.drop(columns = ['num_passengers','vendor_id','ID','payment_method','stored_flag']) #making my features
+print(x.dtypes)
+print(x['trip_duration'].isna().sum())
+
+for col in x:
+    if x[col].dtype == 'object':
+        x[col] = pd.to_numeric(x[col], errors = 'coerce')
+
+
+y = x['driver_tip'] + x['toll_amount'] + x['extra_charges'] + x['total_amount'] + x['extra_charges'] + x['improvement_charge'] #making the target (i.e. total cost)
+
+
+x = x.drop(columns = ['driver_tip', 'toll_amount', 'extra_charges', 'total_amount', 'extra_charges','improvement_charge', 'mta_tax'])
+
+#x['total_cost'] = y
+
+plt.figure(figsize = (50, 75))
+corr = x.corr()
+sb.heatmap(corr, annot = True)
+plt.yticks(rotation = 0)
+plt.xticks(rotation = 90)
+plt.show()
+
+
+#make the training and test sets
+
